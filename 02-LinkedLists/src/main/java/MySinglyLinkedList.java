a5ef0213a650d04ccf6662a1da96fec099c15058
Rasul <abbosboev@gmail.com>
2022-02-26 11:59:43 -0500

    Linked List module is created.

---
+++ 02-LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -0,0 +1,11 @@
+public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){ return false;};
+    void add(int data){};
+    void printNodes() {};
+    void deleteNode() {};
+    int indexOf(int value){ return 0;};
+}
