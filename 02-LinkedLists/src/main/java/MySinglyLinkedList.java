f308a293d34617808b9a8a6cd7a5db0973951d2d
Rasul <abbosboev@gmail.com>
2022-02-26 13:40:53 -0500

    Create My Single Linked List class

--- 02-LinkedLists/src/main/java/MySinglyLinkedList.java
+++ 02-LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -3,9 +3,60 @@ public class MySinglyLinkedList {
     Node tail;
     int size;
 
-    boolean isEmpty(){ return false;};
-    void add(int data){};
-    void printNodes() {};
-    void deleteNode() {};
-    int indexOf(int value){ return 0;};
+
+    public MySinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    boolean isEmpty(){
+        return head==null;
+    }
+
+//Adds element into last position in the linked list
+    void add(int data){
+        //create a new node with data value
+        Node node= new Node(data);
+        //check if empty
+        if(isEmpty()){
+            head=tail=node;
+        }else{
+            tail.next=node;
+            tail=node;
+        }
+        size++;
+    }
+
+
+    void printNodes() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null)
+                System.out.print(current.value + "=> null");
+             else
+                System.out.print(current.value + "=>");
+                current = current.next;
+
+        }
+    }
+    void deleteNode() {
+
+    }
+
+
+
+    int indexOf(int value){
+        int pos=0;
+        Node current=head;
+        if(isEmpty())
+            return -1;
+        while (current!=null){
+            if(current.value==value)
+                return pos;
+            pos++;
+            current=current.next;
+        }
+       return -1;
+    }
 }
